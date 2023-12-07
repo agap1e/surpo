5fc3159d175f680ae79f12f5525000560ef53e98
sensecreep <lera.grint19@gmail.com>
2023-12-07 16:10:40 +0300

    comm

---
+++ surpo/surpo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace surpo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hi");
+            Console.WriteLine("i'm vers2");
+            Console.WriteLine("i'm vers1");
+            int a = 1;
+            string b = "fnie";
+            //Console.WriteLine(a * (b / 0));
+        }
+    }
+}
